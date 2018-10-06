b3047c35b697cfb815a4fb7d0c2750e7ff7e66e3
Myron Carvalho <myron.c@somaiya.edu>
2018-10-06 19:01:54 +0530

    changes in header. added new font style.

--- inc/header.php
+++ inc/header.php
@@ -1,1 +1,5 @@
 <link rel="stylesheet" href="https://bootswatch.com/4/united/bootstrap.min.css">
+<link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+<style>
+    font-family: 'Pacifico', cursive;
+</style>
